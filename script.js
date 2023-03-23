7165a43179f101a5e1f09e11d627548f87557826
Leonid10011 <leonid.budkov@gmail.com>
2023-03-23 19:15:44 +0100

    Date functions

---
+++ script.js
@@ -0,0 +1,13 @@
+// date $ times
+const now = new Date();
+
+console.log(typeof(now));
+
+// years, month, day, times
+console.log("getFullYear: ", now.getFullYear());
+console.log("getMonth: ", now.getMonth(), " position zero based");
+console.log("getDate: ", now.getDate());
+console.log("getDay: ", now.getDay());
+console.log("getHours: ", now.getHours());
+console.log("getMinutes: ", now.getMinutes());
+console.log("getSeconds: ", now.getSeconds());
