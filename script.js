01b6eb6de005390692fb9d12f32bddddffc9e499
Leonid10011 <leonid.budkov@gmail.com>
2023-03-23 19:26:44 +0100

    Working with timestamps added

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ console.log("Timestamp: ", now.getTime(), " number of miliseconds since january 
 console.log(now.toDateString());
 console.log(now.toTimeString());
 console.log(now.toLocaleString());
+
+// timestamps extended
+
+const before = new Date("February 1 2019 7:30:59");
+const now2 = new Date();
+
+console.log(now2.getTime(), before.getTime());
+
+const diff = now2.getTime() - before.getTime();
+console.log(diff);
+
+const mins = Math.round(diff/1000/60);
+console.log("Past minutes since 2019: ", mins);
+const hours = Math.round(mins / 60);
+console.log("Past hours: since 2019", hours);
+const days = Math.round(hours /24)
+console.log("Past days since 2019", days);
+const years = Math.round(days / 365);
+console.log("Past years since 2019", years);
+
+// converting timestamp into date object
+const timestamp = 1679595942894;
+console.log(new Date(timestamp));
