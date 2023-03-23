dd9094eae9904956ddaca96cc974a957b1d6e27f
Leonid10011 <leonid.budkov@gmail.com>
2023-03-23 19:17:45 +0100

    Date functions

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ console.log("getDay: ", now.getDay());
 console.log("getHours: ", now.getHours());
 console.log("getMinutes: ", now.getMinutes());
 console.log("getSeconds: ", now.getSeconds());
+
+// timestamps
+console.log("Timestamp: ", now.getTime(), " number of miliseconds since january 1970");
+ 
+// date strings
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toLocaleString());
